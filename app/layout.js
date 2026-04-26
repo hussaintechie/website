1eaa9fe3904adfe0c473eb23d7b708281c2467c6
hussaintechie <rajhussain1042003@gmail.com>
2026-04-26 23:03:05 +0530

    chnaged

--- app/layout.js
+++ app/layout.js
@@ -3,8 +3,22 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 export const metadata = {
-  title: 'Join as Call Girl | High Income Jobs in Chennai',
-  description: 'Join a professional companionship service in Chennai. Earn high income with flexible work opportunities and secure support.',
+  title: 'Join as Call Boy in Chennai | High Income Male Escort Jobs',
+  description: 'Looking for call boy jobs in Chennai? Join a professional companionship service with flexible work, secure support, and high earning opportunities.',
+  keywords: [
+    'hire call boy Chennai',
+    'hire call boy ',
+    'call boy in Chennai',
+    'call boy jobs Chennai',
+    'need call boy Chennai',
+    'join call boy service',
+    'male escort jobs Chennai',
+    'call boy opportunities',
+    'high income call boy jobs',
+    'Chennai companionship jobs',
+    'professional male escort Chennai',
+    'part-time call boy jobs Chennai'
+  ],
 }
 
 export default function RootLayout({ children }) {
