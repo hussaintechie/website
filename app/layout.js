bf1c385ce7618dc051fe24c68c45cfdbc7ab8d4d
hussaintechie <rajhussain1042003@gmail.com>
2026-05-05 23:30:49 +0530

    sitemap added

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,14 @@
-import './globals.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import WhatsAppButton from "@/components/WhatsAppButton";
 
 export const metadata = {
-  title: 'Join as Call Boy in Chennai | High Income Male Escort Jobs',
-  description: 'Looking for call boy jobs in Chennai? Join a professional companionship service with flexible work, secure support, and high earning opportunities.',
+  title: "Join as Call Boy in Chennai | High Income Male Escort Jobs",
+  description:
+    "Looking for call boy jobs in Chennai? Join a professional companionship service with flexible work, secure support, and high earning opportunities.",
   keywords: [
-    'hire call boy Chennai',
+     'hire call boy Chennai',
     'hire call boy ',
     'call boy in Chennai',
     'call boy jobs Chennai',
@@ -19,17 +21,50 @@ export const metadata = {
     'professional male escort Chennai',
     'part-time call boy jobs Chennai'
   ],
-}
+  authors: [{ name: "call boy" }],
+  robots: "index, follow",
+
+  icons: {
+    icon: "/favicon.ico",
+  },
+
+  openGraph: {
+    title: "call boy",
+    description:
+      "Looking for call boy jobs in Chennai? Join a professional companionship service with flexible work, secure support, and high earning opportunities.",
+    url: "https://callboy.website",
+    siteName: "Call Boy",
+    type: "website",
+    locale: "en_IN",
+    images: [
+      {
+        url: "https://callboy.website/favicon.ico", // Replace with your actual OG image
+        width: 1200,
+        height: 630,
+        alt: "Call Boy - Join as",
+      },
+    ],
+  },
+  other: {
+  "geo.region": "IN-TN",
+  "geo.placename": "Chennai",
+  "geo.position": "13.0827;80.2707",
+  "ICBM": "13.0827, 80.2707",
+},
+
+  alternates: {
+    canonical: "https://callboy.website",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="antialiased">
+      <body className="bg-slate-50 text-slate-900">
         <Navbar />
-        <main className="min-h-screen">
-          {children}
-        </main>
+        <main className="min-h-screen">{children}</main>
         <Footer />
+        <WhatsAppButton />
       </body>
     </html>
   );
